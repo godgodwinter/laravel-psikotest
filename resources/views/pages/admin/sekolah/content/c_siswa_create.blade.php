c852e1027c9af2eb6dfb7c981a63afc176a0182b
bacanprasetyo <92247271+bacanprasetyo@users.noreply.github.com>
2021-10-16 19:39:13 +0700

    update form siswa

--- resources/views/pages/admin/sekolah/content/c_siswa_create.blade.php
+++ resources/views/pages/admin/sekolah/content/c_siswa_create.blade.php
@@ -16,7 +16,7 @@
                         <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Orang Tua</a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+                        <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Testing branch</a>
                       </li>
                     </ul>
                     <div class="tab-content" id="myTabContent">
