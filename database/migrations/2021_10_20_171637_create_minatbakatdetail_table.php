5f1ef9900c6d1e7158fed6ae47b23022fdec98ca
godgodwinter <kakadz.pro@gmail.com>
2021-10-20 17:20:44 +0700

    add migrasi minat dan bakat

---
+++ database/migrations/2021_10_20_171637_create_minatbakatdetail_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMinatbakatdetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('minatbakatdetail', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('minatbakat_id');
+            $table->string('siswa_id');
+            $table->string('nilai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('minatbakatdetail');
+    }
+}
