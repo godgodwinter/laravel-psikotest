1204c2f11091d5aff5b561de2eefc975b960f349
godgodwinter <kakadz.pro@gmail.com>
2021-10-31 10:27:57 +0700

    update css dan layout sekolah index

--- resources/views/pages/admin/sekolah/index.blade.php
+++ resources/views/pages/admin/sekolah/index.blade.php
@@ -27,9 +27,9 @@ Sekolah
         <div class="card">
             <div class="card-body">
 
-                <div id="babeng-bar" class="text-center mt-2">
+                <div class="d-flex bd-highlight mb-0 align-items-center">
 
-                    <div id="babeng-row ">
+                    <div class="p-2 bd-highlight">
 
                         <form action="{{ route('sekolah.cari') }}" method="GET">
                             {{-- <label for="">Urutkan </label>
@@ -43,11 +43,14 @@ Sekolah
                             </select> --}}
 
                             <input type="text" class="babeng babeng-select  ml-0" name="cari">
-
+                        </div>
+                    <div class="p-2 bd-highlight">
                             <span>
                                 <input class="btn btn-info ml-1 mt-2 mt-sm-0" type="submit" id="babeng-submit"
                                     value="Cari">
                             </span>
+                        </div>
+                    <div class="ml-auto p-2 bd-highlight">
 
                             <a href="{{route('sekolah.create')}}" type="submit" value="Import"
                                 class="btn btn-icon btn-primary btn-sm ml-2"><span class="pcoded-micon"> <i
@@ -69,10 +72,10 @@ Sekolah
                     <x-jsdatatable/>
                 @endif
 
-                <table id="example" class="table table-striped table-bordered mt-1" style="width:100%">
+                <table id="example" class="table table-striped table-bordered table-sm mt-1" style="width:100%">
                     <thead>
                         <tr>
-                            <th width="8%" class="text-center"> <input type="checkbox" id="chkCheckAll"> All</th>
+                            <th class="text-center babeng-min-row"> <input type="checkbox" id="chkCheckAll"> All</th>
                             <th >Nama</th>
                             <th>Alamat</th>
                             <th width="10%" class="text-center">Status</th>
@@ -101,7 +104,7 @@ Sekolah
                                     <div class="badge badge-{{$warna}}">{{$data->status}}</div>
 
                                 </td>
-                                <td class="text-center">
+                                <td class="text-center babeng-min-row">
                                     {{-- <x-button-reset-pass link="/admin/{{ $pages }}/{{$data->id}}/reset" /> --}}
                                     <x-button-edit link="/admin/{{ $pages }}/{{$data->id}}" />
                                     <x-button-delete link="/admin/{{ $pages }}/{{$data->id}}" />
@@ -115,17 +118,22 @@ Sekolah
                     </tbody>
                 </table>
 
-@php
-$cari=$request->cari;
-$tapel_nama=$request->tapel_nama;
-$kelas_nama=$request->kelas_nama;
-@endphp
-{{ $datas->onEachSide(1)
-  ->links() }}
-<a href="#" class="btn btn-sm  btn-danger mb-2" id="deleteAllSelectedRecord"
-            onclick="return  confirm('Anda yakin menghapus data ini? Y/N')"  data-toggle="tooltip" data-placement="top" title="Hapus Terpilih">
-            <i class="fas fa-trash-alt mr-2"></i> Hapus Terpilih</i>
-        </a>
+    <div class="d-flex justify-content-between flex-row-reverse mt-3">
+        <div >
+                    @php
+                    $cari=$request->cari;
+                    $tapel_nama=$request->tapel_nama;
+                    $kelas_nama=$request->kelas_nama;
+                    @endphp
+                    {{ $datas->onEachSide(1)
+                    ->links() }}
+        </div>
+        <div>
+            <a href="#" class="btn btn-sm  btn-danger mb-2" id="deleteAllSelectedRecord"
+                        onclick="return  confirm('Anda yakin menghapus data ini? Y/N')"  data-toggle="tooltip" data-placement="top" title="Hapus Terpilih">
+                        <i class="fas fa-trash-alt mr-2"></i> Hapus Terpilih</i></a>
+        </div>
+    </div>
             </div>
         </div>
     </div>
