029309d2c16f6c0215f3b371053ccf0974bbc6c0
godgodwinter <kakadz.pro@gmail.com>
2021-11-03 13:22:51 +0700

    remove beranda untuk login bk ganti dengan beranda sekolah

--- app/Http/Controllers/admindashboardcontroller.php
+++ app/Http/Controllers/admindashboardcontroller.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class admindashboardcontroller extends Controller
 {
     public function index(){
+        if(Auth::user()->tipeuser=='bk'){
+            return redirect()->route('bk.beranda');
 
+        }
         $pages='dashboard';
         return view('pages.admin.dashboard.index',compact('pages'));
     }
