cf83d2723d26ec1c35f4ac6879fd8e576b785e22
godgodwinter <kakadz.pro@gmail.com>
2021-12-03 21:26:47 +0700

    fix bug api santrum

--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('user', [apilogincontroller::class, 'getuser']);
+// Route::get('user', [apilogincontroller::class, 'getuser']);
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 
 // Route::post('/logout', [apilogincontroller::class, 'logout']);
