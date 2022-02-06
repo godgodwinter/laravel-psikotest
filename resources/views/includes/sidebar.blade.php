f0d4aa5071ceb9e4f2759de6e4ed36db86f90dfc
godgodwinter <kakadz.pro@gmail.com>
2022-02-06 09:11:05 +0700

    update sidebar icon siswa

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -155,15 +155,16 @@
 
                         @elseif((Auth::user()->tipeuser)=='siswa')
                         <li class="menu-header">Menu</li>
-                        <li {{$pages=='dashboard' ? 'class=active' : ''}}><a class="nav-link" href="{{route('dashboard')}}"><i class="fas fa-diagnoses"></i> <span>Beranda</span></a></li>
+                        <li {{$pages=='dashboard' ? 'class=active' : ''}}><a class="nav-link" href="{{route('dashboard')}}"><i
+                            class="fas fa-home"></i> <span>Beranda</span></a></li>
                         <li {{$pages=='deteksi' ? 'class=active' : ''}}><a class="nav-link" href="{{route('siswa.hasilpsikologi.deteksi_lihat')}}"><i class="fas fa-diagnoses"></i> <span>Hasil Deteksi</span></a></li>
-                        <li {{$pages=='sertifikat' ? 'class=active' : ''}}><a class="nav-link" href="{{route('siswa.hasilpsikologi.sertifikat_lihat')}}"><i class="fas fa-stamp"></i> <span>Sertifikat</span></a></li>
+                        <li {{$pages=='sertifikat' ? 'class=active' : ''}}><a class="nav-link" href="{{route('siswa.hasilpsikologi.sertifikat_lihat')}}"><i class="far fa-star"></i> <span>Sertifikat</span></a></li>
 
-                        <li {{$pages=='catatankasus' ? 'class=active' : ''}}><a class="nav-link" href="{{route('siswa.catatankasus')}}"><i class="fas fa-stamp"></i> <span>Catatan Kasus siswa</span></a></li>
+                        <li {{$pages=='catatankasus' ? 'class=active' : ''}}><a class="nav-link" href="{{route('siswa.catatankasus')}}"><i class="fas fa-clipboard"></i> <span>Catatan Kasus siswa</span></a></li>
 
-                        <li {{$pages=='catatanpengembangandirisiswa' ? 'class=active' : ''}}><a class="nav-link" href="{{route('siswa.catatanpengembangandiri')}}"><i class="fas fa-stamp"></i> <span>Catatan Pengembangan Diri Siswa</span></a></li>
+                        <li {{$pages=='catatanpengembangandirisiswa' ? 'class=active' : ''}}><a class="nav-link" href="{{route('siswa.catatanpengembangandiri')}}"><i class="far fa-clipboard"></i> <span>Catatan Pengembangan Diri Siswa</span></a></li>
 
-                        <li {{$pages=='catatanprestasisiswa' ? 'class=active' : ''}}><a class="nav-link" href="{{route('siswa.catatanprestasi')}}"><i class="fas fa-stamp"></i> <span>Catatan Prestasi Siswa</span></a></li>
+                        <li {{$pages=='catatanprestasisiswa' ? 'class=active' : ''}}><a class="nav-link" href="{{route('siswa.catatanprestasi')}}"><i class="fas fa-book-reader"></i> <span>Catatan Prestasi Siswa</span></a></li>
 
                         <li {{$pages=='siswa-klasifikasijabatan' ? 'class=active' : ''}}><a class="nav-link"
                             href="{{route('siswa.klasifikasijabatan')}}"><i class="fas fa-sort-alpha-up"></i>
