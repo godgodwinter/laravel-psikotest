ce2dbf78b774c05d2f52e11471081bda34c2fe12
godgodwinter <kakadz.pro@gmail.com>
2022-03-11 20:26:08 +0700

    fix bug klasifikasi

--- resources/views/pages/admin/klasifikasijabatan/detail.blade.php
+++ resources/views/pages/admin/klasifikasijabatan/detail.blade.php
@@ -48,10 +48,10 @@ Klasifikasi Akademis & Profesi
                     </div>
                 </div>
                 <div class="form-group row align-items-center">
-                    <label for="pekerjaan" class="form-control-label col-sm-3 text-md-right">Akademis </label>
+                    <label for="akademis" class="form-control-label col-sm-3 text-md-right">Akademis </label>
                     <div class="col-sm-6 col-md-9">
 
-                      <input type="text" class="form-control  @error('pekerjaan') is-invalid @enderror" name="pekerjaan" readonly  value="{{old('pekerjaan')!=null?old('pekerjaan'):$data->pekerjaan}}">
+                      <input type="text" class="form-control  @error('pekerjaan') is-invalid @enderror" name="akademis" readonly  value="{{old('akademis')!=null?old('akademis'):$data->akademis}}">
 
                       @error('pekerjaan')<div class="invalid-feedback"> {{$message}}</div>
                       @enderror
