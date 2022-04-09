80546103df6da621b309cf1a670fef2b3ca45d0e
godgodwinter <kakadz.pro@gmail.com>
2022-04-09 15:51:28 +0700

    fix bug ketika null katabijak

--- resources/views/includes/katabijakfixed.blade.php
+++ resources/views/includes/katabijakfixed.blade.php
@@ -1,27 +1,29 @@
 <div class="text-white d-none d-lg-block
  ">
- {{-- d-none d-lg-block --}}
+    {{-- d-none d-lg-block --}}
     <h1 class="ml12">
         <span class="text-wrapper">
             @php
-                $getCount=\App\Models\katabijak::where('status','Ditampilkan')->count();
-                $item=\App\Models\katabijak::where('status','Ditampilkan')->get();
-                $getTgl=date('d');
-                $hasilbagi=($getTgl%$getCount);
-                if($hasilbagi==0)
-                {
-                    $hasilbagi=$getCount;
-                }
+                $getCount = \App\Models\katabijak::where('status', 'Ditampilkan')->count();
             @endphp
-          
-<span class="letters">{{$item[$hasilbagi-1]->judul}} : {{$item[$hasilbagi-1]->penjelasan}} 
-    {{-- - {{$hasilbagi}} --}}
-    {{-- - {{$getCount}} - {{$getTgl}} --}}
-</span>
-     
+            @if ($getCount > 0)
+                @php
+                    $item = \App\Models\katabijak::where('status', 'Ditampilkan')->get();
+                    $getTgl = date('d');
+                    $hasilbagi = $getTgl % $getCount;
+                    if ($hasilbagi == 0) {
+                        $hasilbagi = $getCount;
+                    }
+                @endphp
+
+                <span class="letters">{{ $item[$hasilbagi - 1]->judul }} : {{ $item[$hasilbagi - 1]->penjelasan }}
+                    {{-- - {{$hasilbagi}} --}}
+                    {{-- - {{$getCount}} - {{$getTgl}} --}}
+                </span>
+            @endif
             {{-- <span class="letters">   Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui sint quia sapiente, at soluta magni aut enim fuga veritatis rem asperiores officiis sit aliquam nemo? Iusto quaerat ut eius dicta.
             </span> --}}
         </span>
     </h1>
-  </div>
-  {{-- https://tobiasahlin.com/moving-letters/ --}}
+</div>
+{{-- https://tobiasahlin.com/moving-letters/ --}}
