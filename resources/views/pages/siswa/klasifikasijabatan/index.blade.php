d94e3a66c5a66412fbf9b57d5a26db8a730a3de8
godgodwinter <kakadz.pro@gmail.com>
2022-04-18 20:12:08 +0700

    fix bug pagination klasifikasi jabatan

--- resources/views/pages/siswa/klasifikasijabatan/index.blade.php
+++ resources/views/pages/siswa/klasifikasijabatan/index.blade.php
@@ -1,38 +1,37 @@
 @extends('layouts.default')
 
 @section('title')
-Klasifikasi Akademis dan Profesi
+    Klasifikasi Akademis dan Profesi
 @endsection
 
 @push('before-script')
-
-@if (session('status'))
-<x-sweetalertsession tipe="{{session('tipe')}}" status="{{session('status')}}"/>
-@endif
+    @if (session('status'))
+        <x-sweetalertsession tipe="{{ session('tipe') }}" status="{{ session('status') }}" />
+    @endif
 @endpush
 
 
 @section('content')
-<section class="section">
-    <div class="section-header">
-        <h1>@yield('title')</h1>
-        <div class="section-header-breadcrumb">
-            <div class="breadcrumb-item active"><a href="{{route('dashboard')}}">Dashboard</a></div>
-            {{-- <div class="breadcrumb-item"><a href="#">Layout</a></div> --}}
-            <div class="breadcrumb-item">@yield('title')</div>
+    <section class="section">
+        <div class="section-header">
+            <h1>@yield('title')</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="{{ route('dashboard') }}">Dashboard</a></div>
+                {{-- <div class="breadcrumb-item"><a href="#">Layout</a></div> --}}
+                <div class="breadcrumb-item">@yield('title')</div>
+            </div>
         </div>
-    </div>
 
-    <div class="section-body">
-        <div class="card">
-            <div class="card-body">
+        <div class="section-body">
+            <div class="card">
+                <div class="card-body">
 
-                <div class="d-flex bd-highlight mb-0 align-items-center">
+                    <div class="d-flex bd-highlight mb-0 align-items-center">
 
-                    <div class="p-2 bd-highlight">
+                        <div class="p-2 bd-highlight">
 
-                        <form action="{{ route('siswa.klasifikasijabatan.cari') }}" method="GET">
-                            {{-- <label for="">Urutkan </label>
+                            <form action="{{ route('siswa.klasifikasijabatan.cari') }}" method="GET">
+                                {{-- <label for="">Urutkan </label>
                             <select class="babeng babeng-select  ml-2" name="pelajaran_nama">
 
                                 <option>Terbaru</option>
@@ -42,15 +41,14 @@ Klasifikasi Akademis dan Profesi
                                 <option>Z - A</option>
                             </select> --}}
 
-                            <input type="text" class="babeng babeng-select  ml-0" name="cari">
+                                <input type="text" class="babeng babeng-select  ml-0" name="cari">
                         </div>
-                    <div class="p-2 bd-highlight">
+                        <div class="p-2 bd-highlight">
                             <span>
-                                <input class="btn btn-info ml-1 mt-2 mt-sm-0" type="submit" id="babeng-submit"
-                                    value="Cari">
+                                <input class="btn btn-info ml-1 mt-2 mt-sm-0" type="submit" id="babeng-submit" value="Cari">
                             </span>
                         </div>
-                    <div class="ml-auto p-2 bd-highlight">
+                        <div class="ml-auto p-2 bd-highlight">
 
                             {{-- <a href="{{route('siswa.klasifikasijabatan.create')}}" type="submit" value="Import"
                                 class="btn btn-icon btn-primary btn-sm ml-2"><span class="pcoded-micon"> <i
@@ -62,101 +60,101 @@ Klasifikasi Akademis dan Profesi
                             <a href="/admin/klasifikasijabatan/export" type="submit" value="Import"
                                 class="btn btn-icon btn-primary btn-sm mr-2"><span class="pcoded-micon"> <i
                                         class="fas fa-download"></i> Export </span></a> --}}
-                        </form>
+                            </form>
 
+                        </div>
                     </div>
-                </div>
 
-                {{-- <x-jsmultidel link="{{route('siswa.klasifikasijabatan.multidel')}}" />
-                @if($datas->count()>0)
+                    {{-- <x-jsmultidel link="{{route('siswa.klasifikasijabatan.multidel')}}" />
+                @if ($datas->count() > 0)
                     <x-jsdatatable/>
                 @endif --}}
 
-                <table id="example" class="table table-striped table-bordered mt-1 table-sm" style="width:100%">
-                    <thead>
-                        <tr>
-                            <th class="text-center babeng-min-row"> <input type="checkbox" id="chkCheckAll"> All</th>
-                            <th >Bidang</th>
-                            <th >Akademis</th>
-                            <th >Profesi</th>
-                            <th >Nilai Standart</th>
-                            <th >IQ Standart</th>
-                            <th >Jurusan & Bidang Studi yang ditekuni</th>
-                            <th >Pekerjaan & Keterangan</th>
-                            <th >Link</th>
-                            <th >Detail</th>
-                            {{-- <th class="text-center">Aksi</th> --}}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($datas as $data)
-                        <tr id="sid{{ $data->id }}">
-                                <td class="text-center">
-                                    <input type="checkbox" name="ids" class="checkBoxClass " value="{{ $data->id }}">
-                                    {{ ((($loop->index)+1)+(($datas->currentPage()-1)*$datas->perPage())) }}</td>
-                                <td> {{Str::limit($data->bidang,25,' ...')}}
-                                </td>
-                                <td class="text-center">
-                                    {{Str::limit($data->akademis,25,' ...')}}
-                                </td>
-                                <td class="text-center">
-                                    {{Str::limit($data->pekerjaan,25,' ...')}}
-                                </td>
-                                <td class="text-center">
-                                    {{Str::limit($data->nilaistandart,25,' ...')}}
-                                </td>
-                                <td class="text-center">
-                                    {{Str::limit($data->iqstandart,25,' ...')}}
-                                </td>
-                                <td class="text-center">
-                                    {{Str::limit($data->jurusan,25,' ...')}}
-                                </td>
-                                <td class="text-center">
-                                    {{Str::limit($data->bidangstudi,25,' ...')}}
-                                </td>
-                                <td class="text-center">
-                                    {{Str::limit($data->ket,25,' ...')}}
-                                </td>
-                                <td>
-                                    <a href="{{ route('siswa.klasifikasijabatan.detail',$data->id)}}" class="btn btn-icon btn-info btn-sm ml-1"  data-toggle="tooltip" data-placement="top" title="Lihat data!"><i class="fas fa-angle-double-right"></i></a>
-                                </td>
+                    <table id="example" class="table table-striped table-bordered mt-1 table-sm" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th class="text-center babeng-min-row"> <input type="checkbox" id="chkCheckAll"> All</th>
+                                <th>Bidang</th>
+                                <th>Akademis</th>
+                                <th>Profesi</th>
+                                <th>Nilai Standart</th>
+                                <th>IQ Standart</th>
+                                <th>Jurusan & Bidang Studi yang ditekuni</th>
+                                <th>Pekerjaan & Keterangan</th>
+                                <th>Link</th>
+                                <th>Detail</th>
+                                {{-- <th class="text-center">Aksi</th> --}}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @forelse ($datas as $data)
+                                <tr id="sid{{ $data->id }}">
+                                    <td class="text-center">
+                                        <input type="checkbox" name="ids" class="checkBoxClass "
+                                            value="{{ $data->id }}">
+                                        {{ $loop->index + 1 + ($datas->currentPage() - 1) * $datas->perPage() }}
+                                    </td>
+                                    <td> {{ Str::limit($data->bidang, 25, ' ...') }}
+                                    </td>
+                                    <td class="text-center">
+                                        {{ Str::limit($data->akademis, 25, ' ...') }}
+                                    </td>
+                                    <td class="text-center">
+                                        {{ Str::limit($data->pekerjaan, 25, ' ...') }}
+                                    </td>
+                                    <td class="text-center">
+                                        {{ Str::limit($data->nilaistandart, 25, ' ...') }}
+                                    </td>
+                                    <td class="text-center">
+                                        {{ Str::limit($data->iqstandart, 25, ' ...') }}
+                                    </td>
+                                    <td class="text-center">
+                                        {{ Str::limit($data->jurusan, 25, ' ...') }}
+                                    </td>
+                                    <td class="text-center">
+                                        {{ Str::limit($data->bidangstudi, 25, ' ...') }}
+                                    </td>
+                                    <td class="text-center">
+                                        {{ Str::limit($data->ket, 25, ' ...') }}
+                                    </td>
+                                    <td>
+                                        <a href="{{ route('siswa.klasifikasijabatan.detail', $data->id) }}"
+                                            class="btn btn-icon btn-info btn-sm ml-1" data-toggle="tooltip"
+                                            data-placement="top" title="Lihat data!"><i
+                                                class="fas fa-angle-double-right"></i></a>
+                                    </td>
 
-                                {{-- <td class="text-center babeng-min-row">
+                                    {{-- <td class="text-center babeng-min-row">
                                     <x-button-reset-pass link="/admin/{{ $pages }}/{{$data->id}}/reset" />
                                     <x-button-edit link="{{ route('siswa.klasifikasijabatan.edit',$data->id)}}" />
                                     <x-button-delete link="{{ route('siswa.klasifikasijabatan.destroy',$data->id)}}" />
                                 </td> --}}
-                            </tr>
-                @empty
-                            <tr>
-                                <td colspan="8" class="text-center">Data tidak ditemukan</td>
-                            </tr>
-                @endforelse
-                    </tbody>
-                </table>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="8" class="text-center">Data tidak ditemukan</td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
 
-                {{-- <div class="d-flex justify-content-between flex-row-reverse mt-3">
-                    <div >
-@php
-$cari=$request->cari;
-$tapel_nama=$request->tapel_nama;
-$kelas_nama=$request->kelas_nama;
-@endphp
 
-{{ $datas->onEachSide(1)
+                    <div class="d-flex justify-content-between flex-row-reverse mt-3">
+                        <div>
+                            @php
+                                $cari = $request->cari;
+                                $tapel_nama = $request->tapel_nama;
+                                $kelas_nama = $request->kelas_nama;
+                            @endphp
 
-  ->links() }}
+                            {{ $datas->onEachSide(1)->links() }}
+                        </div>
+                        {
+                        <div>
+
+                        </div>
                     </div>
-{
-<div>
-<a href="#" class="btn btn-sm  btn-danger mb-2" id="deleteAllSelectedRecord"
-            onclick="return  confirm('Anda yakin menghapus data ini? Y/N')"  data-toggle="tooltip" data-placement="top" title="Hapus Terpilih">
-            <i class="fas fa-trash-alt mr-2"></i> Hapus Terpilih</i>
-        </a>
+                </div>
             </div>
-            </div>
-            </div> --}}
-        </div>
-    </div>
-</section>
+    </section>
 @endsection
